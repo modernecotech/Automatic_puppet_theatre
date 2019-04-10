3a1ff713ed78501d56ea18e92664dd6176fe4b30
Hayder <hayder@modernecotech.com>
2019-04-10 21:44:18 +0100

    Add files via upload

--- puppet_robot.py
+++ puppet_robot.py
@@ -1,17 +1,37 @@
 import PySimpleGUI as sg 
 import eng_to_ipa as ipa 
+import csv
 import sys
 
-layout = [[sg.Text('My one-shot window.')],      
-                 [sg.InputText()],      
-                 [sg.Submit(), sg.Cancel()]]      
+layout = [[sg.Text('Open Script file. It should be a CSV generated from the template')],      
+                 [sg.Input(key='_filename_'),sg.FileBrowse()],      
+                 [sg.Button('Process Script'), sg.Text('This may take a while!')],
+                 [sg.Text('_'  * 40, size=(30, 1))],
+                 [sg.Text('Once the processing is complete you can start the show')],
+                 [sg.Button('Start'),sg.Button('Pause'),sg.Button('Cancel')]      
+                 ]      
 
-window = sg.Window('Puppet Speech Robot').Layout(layout)    
+window = sg.Window('Robot Theatre System').Layout(layout)    
+window.Location=(0,0)
 
-event, values = window.Read()    
+def ProcessScript():
+    text_input = values[0] 
+    ipa_text = ipa.convert(text_input)   
+    print(ipa_text)
+
+while True:
+    event, values = window.ReadNonBlocking() #
+
+    if event == 'Quit':
+        break
+    
+    if event == 'Process Script':
+        ProcessScript()
+    
+
+
+
+
 window.Close()
 
-text_input = values[0] 
-ipa_text = ipa.convert(text_input)   
-print(ipa_text)
 #final test routine 1
